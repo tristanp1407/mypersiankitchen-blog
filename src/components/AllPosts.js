6071c925f7a216a2e4cba8c824dd190f26a8ddb4
Tristan Plet <tristanplet@Tristans-MacBook-Pro-2.local>
2021-04-08 08:43:49 +0100

    fixed styles

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import sanityClient from "../client.js";
 import imageUrlBuilder from "@sanity/image-url";
 import * as S from "../components/styles/AllPosts.styles";
+import styled from "styled-components";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -30,21 +31,67 @@ export default function AllPosts() {
       .catch(console.error);
   }, []);
 
+  const AllPostsWrapper = styled("div")`
+    margin: 50px 0;
+    max-width: 700px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    flex-direction: row;
+  `;
+
+  const BlogTile = styled("div")`
+    position: relative;
+    width: 330px;
+    overflow: hidden;
+    height: 250px;
+    transition: 0.3s ease;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 5px;
+  `;
+
+  const BlogTileImage = styled("img")`
+    opacity: 0.6;
+    transition: 0.3s ease;
+    height: 100%;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  `;
+
+  const BlogTitle = styled("h2")`
+    @import url("https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300&display=swap");
+    font-family: "Playfair Display", serif;
+    font-weight: 400;
+    margin: 15px;
+    color: white;
+    position: absolute;
+    text-decoration: none;
+    max-width: 300px;
+    top: 0;
+    left: 0;
+  `;
+
   return (
-    <S.AllPostsWrapper>
+    <AllPostsWrapper>
       {allPostsData &&
         allPostsData.map((post) => (
           <Link to={"/" + post.slug.current} key={post.slug.current}>
-            <S.BlogTile>
-              <S.BlogTileImage
+            <BlogTile>
+              <BlogTileImage
                 src={urlFor(post.mainImage).width(330).url()}
                 alt="main image"
               />
 
-              <S.BlogTitle>{post.title}</S.BlogTitle>
-            </S.BlogTile>
+              <BlogTitle>{post.title}</BlogTitle>
+            </BlogTile>
           </Link>
         ))}
-    </S.AllPostsWrapper>
+    </AllPostsWrapper>
   );
 }
