2ca43c29fe2dfef3d63a3697215251831ba702ff
Tristan Plet <tristanplet@Tristans-MacBook-Pro-2.local>
2021-04-08 08:38:56 +0100

    styles inside component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import phoneIcon from "../Assets/Icons/Contact/phone.png";
 import emailIcon from "../Assets/Icons/Contact/email.png";
 import pinIcon from "../Assets/Icons/Contact/location.png";
 import hygienRating from "../Assets/Icons/Contact/hygiene-rating.jpg";
-import * as S from "../components/styles/Footer.styles";
+// import * as S from "../components/styles/Footer.styles";
+import styled from "styled-components";
 import * as T from "../components/styles/Text.styles";
 
 export default function Footer() {
@@ -17,45 +18,140 @@ export default function Footer() {
     window.open(link, "_blank");
   };
 
-  let SS = S;
-  let TT = T;
+  const FooterBox = styled("div")`
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: black;
+    color: white;
+    margin-top: 70px;
+    font-size: 90%;
+    padding-top: 20px;
+  `;
+
+  const Contact = styled("div")`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    max-width: 100vw;
+  `;
+
+  const IconAndText = styled("div")`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 10px;
+    cursor: pointer;
+    font-weight: 300;
+
+    &:hover {
+      color: rgb(224, 224, 224);
+    }
+
+    & img {
+      margin-top: auto;
+      height: 35px;
+    }
+
+    & div {
+      text-align: left;
+    }
+  `;
+
+  const AdressLine = styled("div")`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    max-width: 80%;
+    width: max-content;
+    margin: 15px auto 8px auto;
+    cursor: pointer;
+    font-weight: 300;
+
+    &:hover {
+      color: rgb(224, 224, 224);
+    }
+
+    & img {
+      margin-top: auto;
+      margin-right: 10px;
+      height: 35px;
+    }
+
+    & div {
+      text-align: left;
+      width: 180px;
+    }
+  `;
+
+  const HygienRatingWrapper = styled("div")`
+    height: 100px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 19px 0 10px 0;
+  `;
+
+  const Me = styled("div")`
+    font-size: 70%;
+    color: grey;
+    padding-bottom: 10px;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 0;
+    bottom: 0px;
+
+    & a {
+      transition: 0.5s ease;
+      color: white;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  `;
 
   return (
-    <SS.FooterBox>
-      <TT.H1>GET IN TOUCH!</TT.H1>
+    <FooterBox>
+      <T.H1>GET IN TOUCH!</T.H1>
 
-      <TT.P>
+      <T.P>
         We'd love to hear from you. For all enquiries, please call us or email
         us:
-      </TT.P>
+      </T.P>
 
-      <SS.Contact>
-        <SS.IconAndText onClick={() => openLink(contactLinks.email)}>
+      <Contact>
+        <IconAndText onClick={() => openLink(contactLinks.email)}>
           <img alt="email icon" src={emailIcon} />
           <div>mpkcontactus@gmail.com</div>
-        </SS.IconAndText>
+        </IconAndText>
 
-        <SS.IconAndText onClick={() => openLink(contactLinks.phone)}>
+        <IconAndText onClick={() => openLink(contactLinks.phone)}>
           <img alt="phone icon" src={phoneIcon} />
           <div>07580 801905</div>
-        </SS.IconAndText>
-      </SS.Contact>
+        </IconAndText>
+      </Contact>
 
-      <SS.AdressLine onClick={() => openLink(contactLinks.map)}>
+      <AdressLine onClick={() => openLink(contactLinks.map)}>
         <img alt="pin icon" src={pinIcon} />
         <div>Hope Street Yard, Hope St, Cambridge, CB1 3NA</div>
-      </SS.AdressLine>
+      </AdressLine>
 
-      <SS.HygienRatingWrapper>
+      <HygienRatingWrapper>
         <img alt="hygiene-rating" src={hygienRating} />
-      </SS.HygienRatingWrapper>
+      </HygienRatingWrapper>
 
-      <SS.Me>
+      <Me>
         <p>
           designed by Tristan Plet -
           <a href="mailto:tristan.plet@gmail.com">tristan.plet@gmail.com</a>
         </p>
-      </SS.Me>
-    </SS.FooterBox>
+      </Me>
+    </FooterBox>
   );
 }
