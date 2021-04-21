9a2b20abf48adaa51a6873dcdd901f21db3237f4
Tristan Plet <tristanplet@Tristans-MacBook-Pro-2.local>
2021-04-21 19:41:09 +0100

    clickable logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import * as S from "./styles/Logo.styles";
 import MpkLogo from "../Assets/Logo/mpk_logo.png";
 
 export default function Logo() {
   return (
-    <S.Logo>
-      <img src={MpkLogo} alt="mpk-logo" />
-    </S.Logo>
+    <Link to={"/"}>
+      <S.Logo>
+        <img src={MpkLogo} alt="mpk-logo" />
+      </S.Logo>
+    </Link>
   );
 }
--- src/components/styles/Text.styles.js
+++ src/components/styles/Text.styles.js
@@ -1,30 +1,26 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
+export const P = styled("p")`
+  @import url("https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300&display=swap");
+  font-family: "Raleway", sans-serif;
+  font-weight: 300;
+  line-height: 2.5rem;
+  font-size: 1.25rem;
+  letter-spacing: 0.025rem;
+`;
 
+export const Title = styled("h1")`
+  @import url("https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300&display=swap");
+  font-size: 3.5rem;
+  font-family: "Playfair Display", serif;
+  font-weight: 400;
+  margin: 0 !important;
+`;
 
-export const P = styled('p')`
-    @import url('https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300&display=swap');   
-    font-family: 'Raleway', sans-serif;
-    font-weight: 300;
-    line-height: 2.5rem;
-    font-size: 1.25rem;
-    letter-spacing: 0.025rem;
-    text-align: center;
-`
-
-export const Title = styled('h1')`
-    @import url('https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300&display=swap');   
-    font-size: 3.5rem;
-    font-family: 'Playfair Display', serif;
-    font-weight: 400;
-    margin: 0 !important;
-    text-align: center;
-`
-
-export const H1 = styled('h1')`
-    @import url('https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300&display=swap');   
-    /* font-size: 3.5rem; */
-    font-family: 'Playfair Display', serif;
-    font-weight: 400;
-    /* margin: 0 !important */
-`
+export const H1 = styled("h1")`
+  @import url("https://fonts.googleapis.com/css2?family=Playfair+Display&family=Raleway:wght@300&display=swap");
+  /* font-size: 3.5rem; */
+  font-family: "Playfair Display", serif;
+  font-weight: 400;
+  /* margin: 0 !important */
+`;
