493ebef58eeb0bc424ead77a2a657e3811cd1a97
tristanp1407 <tristan.plet@gmail.com>
2021-06-23 11:19:30 +0100

    unused vars fix

--- src/components/FollowUs.js
+++ src/components/FollowUs.js
@@ -3,7 +3,7 @@ import * as S from "./styles/FollowUs.styles";
 import * as T from "./styles/Text.styles";
 
 import facebook from "../Assets/Icons/Social/fb_logo.png";
-import deliveroo from "../Assets/Icons/Social/deliveroo_logo.png";
+// import deliveroo from "../Assets/Icons/Social/deliveroo_logo.png";
 import instagram from "../Assets/Icons/Social/insta_logo.png";
 import twitter from "../Assets/Icons/Social/twitter_logo.png";
 import whatsapp from "../Assets/Icons/Social/whatsapp_logo.png";
@@ -18,8 +18,8 @@ export default function SocialMedia() {
     instagram: "https://www.instagram.com/my.persiankitchen/",
     twitter: "https://twitter.com/mypersiancamb?lang=en",
     whatsapp: "https://wa.me/00447580801905",
-    deliveroo:
-      "https://deliveroo.co.uk/menu/cambridge/cambridge-centre/my-persian-kitchen",
+    // deliveroo:
+    //   "https://deliveroo.co.uk/menu/cambridge/cambridge-centre/my-persian-kitchen",
   };
 
   return (
