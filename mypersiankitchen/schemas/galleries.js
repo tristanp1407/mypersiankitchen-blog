94b63dcab21a1afe6a6e654ca232aeef5b91d133
Tristan Plet <tristan.plet@octopusev.com>
2021-11-01 22:09:13 +0000

    Change photos

---
+++ mypersiankitchen/schemas/galleries.js
@@ -0,0 +1,55 @@
+export default {
+  name: "gallery1",
+  title: "Image Gallery 1",
+  type: "document",
+  fields: [
+    {
+      name: "image1",
+      title: "Image 1",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "image2",
+      title: "Image 2",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "image3",
+      title: "Image 3",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "image4",
+      title: "Image 4",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "image5",
+      title: "Image 5",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "image6",
+      title: "Image 6",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
--- src/components/PhotoGalleries.js
+++ src/components/PhotoGalleries.js
@@ -1,8 +1,8 @@
 import mpk15 from "../Assets/Gallery/mpk15.jpg";
-import mpk10 from "../Assets/Gallery/mpk10.jpg";
-import mpk9 from "../Assets/Gallery/mpk9.jpg";
+import mpk30 from "../Assets/Gallery/mpk30.jpg";
+import mpk31 from "../Assets/Gallery/mpk31.jpg";
 import mpk27 from "../Assets/Gallery/mpk27.jpg";
-import mpk6 from "../Assets/Gallery/mpk6.jpg";
+import mpk32 from "../Assets/Gallery/mpk32.jpg";
 import mpk29 from "../Assets/Gallery/mpk29.jpg";
 import mpk11 from "../Assets/Gallery/mpk11.jpg";
 import mpk16 from "../Assets/Gallery/mpk16.jpg";
@@ -13,7 +13,7 @@ import mpk22 from "../Assets/Gallery/mpk22.jpg";
 import React from "react";
 import * as S from "./styles/Gallery.styles";
 
-let galleryImages1 = [mpk15, mpk10, mpk9, mpk27, mpk6, mpk29];
+let galleryImages1 = [mpk15, mpk30, mpk31, mpk27, mpk32, mpk29];
 let galleryImages2 = [mpk11, mpk16, mpk14, mpk23, mpk28, mpk22];
 
 export let gallery1 = galleryImages1.map((image, index) => {
