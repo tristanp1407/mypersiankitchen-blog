e1ec54b53901b08a916e3e6e39cebc884866f046
Tristan Plet <tristan.plet@octopusev.com>
2021-11-25 13:18:49 +0000

    Update phone number

--- src/common/contacts.js
+++ src/common/contacts.js
@@ -2,6 +2,6 @@
 //export const phoneNumber = "07580801905";
 
 //Sirous phone number
-export const phoneNumber = "07807462252";
+export const phoneNumber = "07580801905";
 
-export const whatappPhoneNumber = "447807462252";
+export const whatappPhoneNumber = "447580801905";
