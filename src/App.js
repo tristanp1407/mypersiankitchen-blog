afc558566ebed696c6fe5a1c034ecdf4724ce008
Tristan P <tristan@mybalto.com>
2026-05-12 11:26:05 -0600

    Revert "Reapply "Show "Closed for now" page""
    This reverts commit b0fc04b0620348cc7a5fd514d44caed639a7dd9f.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
-import Closed from "./components/Closed.js";
+import { BrowserRouter, Route } from "react-router-dom";
+import OnePost from "./components/OnePost.js";
+import Home from "./components/Home.js";
+import PopUp from "./components/PopUp.js";
 
 function App() {
-  return <Closed />;
+  return (
+    <BrowserRouter>
+      <div>
+        <Route component={Home} path="/" exact />
+        <Route component={OnePost} path="/:slug" />
+        <PopUp />
+      </div>
+    </BrowserRouter>
+  );
 }
-
 export default App;
